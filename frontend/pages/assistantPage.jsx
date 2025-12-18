6eb3b8347fdf3b9a2892bd51528bc839cdfcbdff
Yuhong11111 <nieyuhong2000@gmail.com>
2025-12-18 14:37:47 -0500

    setup cors

--- frontend/pages/assistantPage.jsx
+++ frontend/pages/assistantPage.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 export default function AssistantPage() {
+    const [healthStatus, setHealthStatus] = useState('Checking API...')
+
+    useEffect(() => {
+        axios
+            .get('health')
+            .then((response) => {
+                setHealthStatus(`API status: ${response.data.status}`)
+            })
+            .catch(() => {
+                setHealthStatus('Unable to reach API')
+            })
+    }, [])
+
     return (
         <div>
             <h1>Assistant Page</h1>
+            <p>{healthStatus}</p>
         </div>
     );
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AssistantPage from '../pages/assistantPage'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:8000'
 
 function App() {
   return (
